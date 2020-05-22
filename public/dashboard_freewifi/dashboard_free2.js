cc89f461032ba8f5b77477f6371ceba599cf64c1
Maria Alejandra <msandoval@sitwifi.com>
2020-05-22 05:30:28 -0500

    js seccionar

--- public/dashboard_freewifi/dashboard_free2.js
+++ public/dashboard_freewifi/dashboard_free2.js
@@ -27,6 +27,60 @@ $(function() {
   });
 });
 
+/* ------- Users ----- */
+
+$( "#pl_age" ).click(function() {
+    $('#panelAges').removeClass("d-none");
+    $('#panelGenders').addClass("d-none");
+    $('#panelDomains').css("opacity", 0);
+});
+
+$( "#pl_gender" ).click(function() {
+    $('#panelAges').addClass("d-none");
+    $('#panelGenders').removeClass("d-none");
+    $('#panelDomains').css("opacity", 0);
+});
+
+$( "#pl_domains" ).click(function() {
+    $('#panelAges').addClass("d-none");
+    $('#panelGenders').addClass("d-none");
+    $('#panelDomains').css("opacity", 1);
+});
+
+
+/* ------- Devices ----- */
+
+$( "#pl_device" ).click(function() {
+    $('#panelDevices').removeClass("d-none");
+    $('#panelBrowsers').addClass("d-none");
+    $('#panelPlataform').addClass("d-none");
+    $('#panelLanguages').addClass("d-none");
+});
+
+$( "#pl_browser" ).click(function() {
+    $('#panelDevices').addClass("d-none");
+    $('#panelBrowsers').css("opacity", 1).removeClass("d-none");
+    $('#panelPlataform').addClass("d-none");
+    $('#panelLanguages').addClass("d-none");
+});
+
+$( "#pl_plataform" ).click(function() {
+    $('#panelDevices').addClass("d-none");
+    $('#panelBrowsers').addClass("d-none");
+    $('#panelPlataform').css("opacity", 1).removeClass("d-none");
+    $('#panelLanguages').addClass("d-none");
+});
+
+$( "#pl_language" ).click(function() {
+    $('#panelDevices').addClass("d-none");
+    $('#panelBrowsers').addClass("d-none");
+    $('#panelPlataform').addClass("d-none");
+    $('#panelLanguages').css("opacity", 1).removeClass("d-none");
+});
+
+
+
+
 $(".select2").select2({
   theme: 'bootstrap',
   placeholder: 'Elije...',
@@ -34,6 +88,7 @@ $(".select2").select2({
   width: 'auto'
 });
 $('#select_scope').on('change', function(){
+
   var id= $(this).val();
   var _token = $('input[name="_token"]').val();
   var datax = [];
@@ -83,8 +138,26 @@ $('.btn_search').on('click', function(){
   graph_devices();
   graph_genders();
   graph_languagues();
-  get_sessions()
+  get_sessions();
 });
+
+$('#btn_search_sessions').on('click', function(){
+    get_sessions();
+});
+
+$('#btn_search_users').on('click', function(){
+    graph_genders();
+    graph_ages();
+    graph_domains();
+});
+
+$('#btn_search_devices').on('click', function(){
+    graph_devices();
+    graph_platforms();
+    graph_browsers();
+    graph_languagues();
+});
+
 
 function graph_browsers() {
   var objData = $('#generate_graphs').find("select,textarea, input").serialize();
@@ -203,6 +276,57 @@ function graph_devices() {
   });
 }
 
+function graph_ages() {
+    var objData = $('#generate_graphs').find("select,textarea, input").serialize();
+    //var form = $('#generate_graphs')[0];
+    //var formData = new FormData(form);
+    var data_count1 = [];
+    var data_name1 = [];
+    var data_group = [];
+    // var data_count1 = [{value:98, name:'Promotores = 98'},{value:62, name:'Pasivos = 62'},{value:21, name:'Detractores = 21'}];
+    // var data_name1 = ["Promotores = 98","Pasivos = 62","Detractores = 21"];
+    $.ajax({
+        type: "POST",
+        url: "/get_graph_ages",
+        data: objData,
+        //contentType: false,
+        //processData: false,
+        success: function (data){
+            //console.log(data); return false;
+            //Cantidad
+            //browser
+            $.each(data,function(index, objdata){
+                //console.log(objdata);
+                data_name1.push(objdata.age + ' = ' + objdata.Cantidad);
+                data_count1.push({ value: objdata.Cantidad, name: objdata.age + ' = ' + objdata.Cantidad},);
+                data_group.push({ages : objdata.age, totals: objdata.Cantidad});
+                //$.each(objdata, function(index, objdata1){
+                //console.log(objdata1);
+                //});
+            });
+
+            var group = {categorias: [], total: []};
+
+            $.each(data, function(key, obj){
+
+                group.categorias.push(obj.label);
+                group.total.push(obj.total);
+
+            });
+
+            //console.log(data);
+            //console.log(group);
+
+            graph_barras_horizontal('maingraphicAges', group);
+
+
+        },
+        error: function (data) {
+            console.log('Error:', data);
+        }
+    });
+}
+
 function graph_genders() {
   var objData = $('#generate_graphs').find("select,textarea, input").serialize();
   //var form = $('#generate_graphs')[0];
@@ -217,22 +341,24 @@ function graph_genders() {
       data: objData,
       //contentType: false,
       //processData: false,
+
+
       success: function (data){
-        //console.log(data);
         //console.log(data[0][0].Cantidad);
         //Cantidad
         //browser
         $.each(data,function(index, objdata){
           //console.log(objdata);
           data_name1.push(objdata.gender + ' = ' + objdata.Cantidad);
-          data_count1.push({ value: objdata.Cantidad, name: objdata.gender + ' = ' + objdata.Cantidad},);
-
+          data_count1.push({ value: objdata.Cantidad, name: objdata.gender, label: objdata.gender + ' = ' + objdata.Cantidad},);
           //$.each(objdata, function(index, objdata1){
             //console.log(objdata1);
           //});
         });
 
-        graph_pie_default_four_with_porcent('maingraphicGenders', data_name1, data_count1, 'Generos', '');
+        graph_table_gender(data_count1);
+
+        //graph_pie_default_four_with_porcent('maingraphicGenders', data_name1, data_count1, 'Generos', '');
 
       },
       error: function (data) {
@@ -278,18 +404,66 @@ function graph_languagues() {
       }
   });
 }
+
+function graph_domains() {
+    var objData = $('#generate_graphs').find("select,textarea, input").serialize();
+    //var form = $('#generate_graphs')[0];
+    //var formData = new FormData(form);
+    var data_count1 = [];
+    var data_name1 = [];
+    // var data_count1 = [{value:98, name:'Promotores = 98'},{value:62, name:'Pasivos = 62'},{value:21, name:'Detractores = 21'}];
+    // var data_name1 = ["Promotores = 98","Pasivos = 62","Detractores = 21"];
+    $.ajax({
+        type: "POST",
+        url: "/get_graph_domains",
+        data: objData,
+        //contentType: false,
+        //processData: false,
+        success: function (data){
+            //console.log(data);
+            //console.log(data[0][0].Cantidad);
+            //Cantidad
+            //browser
+            var categorias = [];
+            var total = [];
+
+            $.each(data,function(index, objdata){
+                //console.log(objdata);
+
+                categorias.push(objdata.name);
+                total.push({value: objdata.total, itemStyle:{color: objdata.color} });
+
+                //$.each(objdata, function(index, objdata1){
+                //console.log(objdata1);
+                //});
+            });
+
+            console.log(total);
+
+
+            graph_bar_domains('maingraphicDomains', categorias, total, 'Dominios Correo', 'registrados');
+
+            //graph_pie_default_four_with_porcent('maingraphicDomains', data_name1, data_count1, 'Dominios Correo', 'registrados');
+
+        },
+        error: function (data) {
+            console.log('Error:', data);
+        }
+    });
+}
+
 function get_sessions() {
   //url: "/get_graph_sessions",
   var objData = $('#generate_graphs').find("select,textarea, input").serialize();
   var dataHorario = [];
   var dataTickets = [];
-  
+
   $.ajax({
     url: "/get_graph_sessions",
     type: "POST",
     data: objData,
     success: function (data) {
-      console.log(data)
+      //console.log(data)
       $.each(data, function(index, dataHor2){
         dataHorario.push(dataHor2.horario);
         dataTickets.push(dataHor2.Cantidad);
@@ -301,6 +475,30 @@ function get_sessions() {
     }
   });
 }
+
+function graph_table_gender(data)
+{
+    var total_num = data[0].value+data[1].value;
+    var women_num = data[1].value;
+    var men_num   = data[0].value;
+
+    var women_p   = (women_num * 100) / total_num ;
+    var women_per = women_p.toFixed(1);
+    var men_p     = (men_num * 100) / total_num  ;
+    var men_per   = men_p.toFixed(1);
+
+    $('#maingraphicGender').removeClass('d-none');
+
+    $('#total_men').text(men_per);
+    $('#total_women').text(women_per);
+    $('#total_people').text(total_num);
+
+    $('#panel_men').attr('title', 'Total Hombres: '+men_num);
+    $('#panel_women').attr('title', 'Total Mujeres: '+women_num);
+
+
+}
+
 function graph_pie_default_four_with_porcent(title, campoa, campob, titlepral, subtitulopral){
   var myChart = echarts.init(document.getElementById(title));
   var option = {
@@ -438,7 +636,7 @@ function graph_barras_ocho_b_zendesk(title, dataHorario, dataTickets, titlepral)
         }
     },
     legend: {
-      data:['Tickets Abiertos']
+      data:['Sesiones']
     },
     toolbox: {
         show : false,
@@ -499,7 +697,7 @@ function graph_barras_ocho_b_zendesk(title, dataHorario, dataTickets, titlepral)
     ],
     series : [
       {
-          name:'Tickets Abiertos',
+          name:'Sesiones',
           type:'bar',
           itemStyle: {normal: {areaStyle: {type: 'default'}}},
           data: dataTickets
@@ -513,4 +711,155 @@ function graph_barras_ocho_b_zendesk(title, dataHorario, dataTickets, titlepral)
           myChart.resize();
       }
   });
+}
+
+
+function graph_barras_horizontal(title, data)
+{
+    //console.log(data);
+    var myChart = echarts.init(document.getElementById(title));
+
+    var option = {
+            title : {
+                show: true,
+                text: "Edades",
+                subtext: "registrados",
+                x:'left',
+                textStyle: {
+                    color: '#449D44',
+                    fontStyle: 'normal',
+                    fontWeight: 'normal',
+                    fontFamily: 'sans-serif',
+                    fontSize: 18,
+                    align: 'center',
+                    verticalAlign: 'top',
+                    width: '100%',
+                    textBorderColor: 'transparent',
+                    textBorderWidth: 0,
+                    textShadowColor: 'transparent',
+                    textShadowBlur: 0,
+                    textShadowOffsetX: 0,
+                    textShadowOffsetY: 0,
+                },
+            },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        legend: {
+            data: ['Personas']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true,
+            backgroundColor: '#fff'
+        },
+        color: ['#3398DB'],
+        xAxis: {
+            type: 'value',
+            boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+            type: 'category',
+            data: data.categorias
+        },
+        series: [
+            {
+                name: 'Personas',
+                type: 'bar',
+                data: data.total
+            }
+        ]
+    };
+
+
+
+    myChart.setOption(option);
+
+    $(window).on('resize', function(){
+        if(myChart != null && myChart != undefined){
+            myChart.resize();
+        }
+    });
+
+}
+
+function graph_bar_domains(title, dominios, cantidad, titlepral, subtitulopral){
+
+    var myChart = echarts.init(document.getElementById(title));
+    var option = {
+        color: ['#9adb60'],
+        title : {
+            show: true,
+            text: titlepral,
+            subtext: subtitulopral,
+            x:'left',
+            textStyle: {
+                color: '#449D44',
+                fontStyle: 'normal',
+                fontWeight: 'normal',
+                fontFamily: 'sans-serif',
+                fontSize: 18,
+                align: 'center',
+                verticalAlign: 'top',
+                width: '100%',
+                textBorderColor: 'transparent',
+                textBorderWidth: 0,
+                textShadowColor: 'transparent',
+                textShadowBlur: 0,
+                textShadowOffsetX: 0,
+                textShadowOffsetY: 0,
+            },
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: dominios,
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [{
+            data: cantidad,
+            type: 'bar'
+        }]
+    };
+
+
+    myChart.setOption(option);
+
+   /* $(window).on('resize', function(){
+        if(myChart != null && myChart != undefined){
+            myChart.resize();
+        }
+    });*/
+
+
+    $(window).on('resize', function(){
+        if(myChart != null && myChart != undefined){
+            myChart.resize();
+        }
+    });
 }
